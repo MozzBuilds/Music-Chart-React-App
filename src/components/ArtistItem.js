857e0629ce777a4507b91b27180dfecd6377c3a6
konrad <ktalaga90@gmail.com>
2021-04-22 17:45:08 +0100

    img added

--- src/components/ArtistItem.js
+++ src/components/ArtistItem.js
@@ -6,6 +6,9 @@ const ArtistItem = ({selectedSong}) => {
             <h2>Selected Song</h2>
         <h3>Artist: {selectedSong.["im:artist"].label}</h3>
         <h4>Title: {selectedSong.["im:name"].label}</h4>
+        <img src={selectedSong.["im:image"][2].label} alt="Album cover"/>
+        <p></p>
+        <p></p>
         </div>
     )
 }
